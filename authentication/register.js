4ab997fb874283b2040697288ffb587df75dbbd3
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-11-12 00:04:33 -0800

    feat: setup passport authentication

---
+++ authentication/register.js
@@ -0,0 +1,22 @@
+const { createUser } = require("../database/queries");
+
+const registerUser = async function (req, res, next) {
+  try {
+    const { first_name, last_name, username, password } = req.body;
+
+    const password_hash = await bycrypt.hash(password, 10);
+
+    res.user = await createUser({
+      first_name,
+      last_name,
+      username,
+      password_hash,
+    });
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = registerUser;
