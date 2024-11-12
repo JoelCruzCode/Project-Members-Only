44540e0039faf10b287fbf043418431ae2e6ccc0
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-11-12 00:05:47 -0800

    feat: add register middleware & route

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { PORT, SESSION_KEY } = process.env;
 const express = require("express");
 const passport = require("passport");
 const session = require("express-session");
@@ -6,8 +7,8 @@ const pg = require("pg-simple-connection")(session);
 const path = require("node:path");
 const flash = require("connect-flash");
 const passport = require("./config/passport");
-const { pool } = require("./database/pool");
-const { PORT, SESSION_KEY } = process.env;
+const pool = require("./database/pool");
+const registerUser = require("./authentication/register");
 
 const app = express();
 
@@ -15,10 +16,16 @@ app.use(express.json());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use((error, req, res, __) => {
+  console.error(error.message);
+  req.flash("error", `An error occured ${error.message}`);
+  res.status(500);
+  // might redirect to a dedicated error page
+});
 
 app.use(
   session({
-    store: new pg({ pool: pool }),
+    store: new pg({ pool: pool, createTableIfMissing: true }),
     secret: SESSION_KEY,
     resave: false,
     saveUninitialized: true,
@@ -34,5 +41,10 @@ app.use(passport.session());
 app.get("/home", render("home-page"));
 
 app.get("/register", render("register"));
+
+app.post("/register", registerUser, (req, res) => {
+  req.flash("success", `Successfully created account ${res.user.username}`);
+  res.redirect("/login");
+});
 
 app.get("/login", render("login"));
