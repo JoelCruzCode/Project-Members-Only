02bfa425e892882484524099b994338ffc90c49a
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-11-12 00:01:03 -0800

    feat: add GetUserById, move pool into pool.js

--- database/pool.js
+++ database/pool.js
@@ -10,45 +10,4 @@ const pool = new Pool({
   port: SQL_PORT,
 });
 
-async function getUser(username) {
-  try {
-    const query = `SELECT username FROM users WHERE username = $1`;
-    const response = await pool.query(query, [username]);
-
-    if (response.rows.length === 0) {
-      return null;
-    }
-    return response.rows[0];
-  } catch (error) {
-    console.log(`Error retrieving user ${username}`, error.message);
-    throw new Error("Internal Server Error");
-  }
-}
-
-async function createUser(info) {
-  try {
-    const { first_name, last_name, username, password_hash } = info;
-    const existingUser = await getUser(username);
-    if (existingUser) {
-      throw new Error(`username ${username} already taken`);
-    }
-    const query = `INSERT INTO users (first_name, last_name, username, password_hash) VALUES ($1, $2, $3, $4) RETURNING * `;
-    const response = await pool.query(query, [
-      first_name,
-      last_name,
-      username,
-      password_hash,
-    ]);
-    return response.rows[0];
-  } catch (error) {
-    console.log("Error inserting user: ", error.message);
-    throw new Error(
-      `Failed to create user. Please try again. Error: ${error.message}`,
-    );
-  }
-}
-
-module.exports = {
-  pool,
-  createUser,
-};
+module.exports = pool;
---
+++ database/queries.js
@@ -0,0 +1,53 @@
+const pool = require("./pool");
+
+async function getUser(username) {
+  try {
+    const query = `SELECT username FROM users WHERE username = $1`;
+    const response = await pool.query(query, [username]);
+
+    if (response.rows.length === 0) {
+      return null;
+    }
+    return response.rows[0];
+  } catch (error) {
+    console.log(`Error retrieving user ${username}`, error.message);
+    throw new Error("Internal Server Error");
+  }
+}
+async function getUserById(id) {
+  try {
+    const query = `SELECT * FROM users WHERE user_id = $1`;
+    const response = await pool.query(query, [id]);
+
+    return response.rows[0];
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createUser(info) {
+  try {
+    const { first_name, last_name, username, password_hash } = info;
+    const existingUser = await getUser(username);
+
+    if (existingUser) {
+      throw new Error(`username ${username} already taken`);
+    }
+    const query = `INSERT INTO users (first_name, last_name, username, password_hash) VALUES ($1, $2, $3, $4) RETURNING * `;
+    const response = await pool.query(query, [
+      first_name,
+      last_name,
+      username,
+      password_hash,
+    ]);
+
+    return response.rows[0];
+  } catch (error) {
+    console.log("Error inserting user: ", error.message);
+    throw new Error(
+      `Failed to create user. Please try again. Error: ${error.message}`,
+    );
+  }
+}
+
+module.exports = { getUser, getUserById, createUser };
